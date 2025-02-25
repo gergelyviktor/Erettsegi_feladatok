4fc1d2d509f3634d43ef61500cb819098ebc7fee
Gergely Viktor <viktor.gergely@gmail.com>
2025-02-25 15:23:03 +0100

    5. kész

--- Otszaz/MainWindow.xaml.cs
+++ Otszaz/MainWindow.xaml.cs
@@ -16,12 +16,12 @@ namespace Otszaz {
     /// </summary>
     public partial class MainWindow : Window {
 
+        List<Dictionary<string, int>> lista = new List<Dictionary<string, int>>();
+        Dictionary<string, int> vasarlas = new Dictionary<string, int>();
         public MainWindow() {
             InitializeComponent();
-            
-            // 1. feladat
-            List<Dictionary<string, int>> lista = new List<Dictionary<string, int>>();
-            Dictionary<string, int> vasarlas = new Dictionary<string, int>();
+
+            // 1. feladat - beolvasás
             string[] sorok = File.ReadAllLines("penztar.txt");
             foreach (var item in sorok) {
                 if (item != "F") {
@@ -38,8 +38,47 @@ namespace Otszaz {
                     vasarlas.Clear();
                 }
             }
-            // 2. feladat
+            // 2. feladat - hányszor fizettek a pénztárnál
             label2.Content = lista.Count;
+            // 3. feladat - az első vásárlónak hány darab árucikk volt a kosarában
+            //label3.Content = lista[0].Count; // ez nem jó, mert a Count csak a kulcsokat számolja, nem a darabszámot
+            var db = 0;
+            foreach (var item in lista[1]) {
+                db += item.Value;
+            }
+            label3.Content = db;
+            // vagy LINQ-val
+            //label3.Content = lista[1].Values.Sum();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            // 4. feladat - kérjük be egy vásárlás sorszámát, egy árucikk nevét és egy darabszámot
+            var sorszam = int.Parse(textbox1.Text);
+            var arucikk = textbox2.Text;
+            var darabszam = int.Parse(textbox3.Text);
+            // 5. feladat - EZ NEHÉZ, KIHAGYNI! Az árucikkből melyik vásárláskor vettek először és melyiknél utoljára
+            // összesen hány alkalommal vásároltak
+            int hanyszor = 1;
+            int elso = 0;
+            int utolso = 0;
+            bool eloszor = true;
+            int mennyi = 0;
+            foreach (var i in lista) {
+                foreach (var j in i) {
+                    if (j.Key == arucikk && eloszor) {
+                        elso = hanyszor;
+                        eloszor = false; // emiatt csak az első találatnál lép be a belső ciklusba
+                    }
+                    if (j.Key == arucikk) {
+                        utolso = hanyszor;
+                        mennyi++;
+                    }
+                }
+                hanyszor++;
+            }
+            label5.Content = elso + " " + utolso + " " + mennyi;
+            // 6. feladat - a bekért darabszámot vásárolva a termékből mennyi a fizetendő összeg
+
         }
     }
 }
