c296b493d4270b046c1deb454436c0bb492774ff
Gergely Viktor <viktor.gergely@gmail.com>
2025-02-25 14:08:03 +0100

    Ötszáz 2-esig kész

--- Otszaz/MainWindow.xaml.cs
+++ Otszaz/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,8 +15,31 @@ namespace Otszaz {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
         public MainWindow() {
             InitializeComponent();
+            
+            // 1. feladat
+            List<Dictionary<string, int>> lista = new List<Dictionary<string, int>>();
+            Dictionary<string, int> vasarlas = new Dictionary<string, int>();
+            string[] sorok = File.ReadAllLines("penztar.txt");
+            foreach (var item in sorok) {
+                if (item != "F") {
+                    if (!vasarlas.ContainsKey(item)) {
+                        vasarlas.Add(item, 1);
+                    }
+                    else {
+                        vasarlas[item]++;
+                    }
+                }
+                else {
+                    //lista.Add(vasarlas); // ez nem jó, mert a Clear innen is kiszedné az értékeket, hiszen nem értékeket töröl, hanem referenciákat
+                    lista.Add(new Dictionary<string, int>(vasarlas));
+                    vasarlas.Clear();
+                }
+            }
+            // 2. feladat
+            label2.Content = lista.Count;
         }
     }
 }
