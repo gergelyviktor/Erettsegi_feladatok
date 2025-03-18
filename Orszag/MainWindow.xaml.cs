f5a82f899b30a733016c91690099d9645e32592c
Gergely Viktor <viktor.gergely@gmail.com>
2025-03-18 13:30:37 +0100

    Orszagok projekt elkezdve

---
+++ Orszag/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Orszagok {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        List<Orszag> adatok = new List<Orszag>();
+        public MainWindow() {
+            InitializeComponent();
+            foreach (var item in File.ReadAllLines("orszag.txt").Skip(1)) {
+                var temp = new Orszag(item);
+                adatok.Add(temp);
+            }
+            dtg1.ItemsSource = adatok;
+        }
+    }
+}
---
+++ Orszag/Orszag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orszagok {
+    internal class Orszag {
+        public string Orszagnev { get; set; }
+        public int Terulet { get; set; }
+        public int Nepesseg { get; set; }
+        public string Fovaros { get; set; }
+        public int FovarosNepesseg { get; set; }
+
+        public Orszag(string adatsor) {
+            string[] sor = adatsor.Split(';');
+            Orszagnev = sor[0];
+            Terulet = int.Parse(sor[1]);
+            if (sor[2].Last() == 'g') {
+                Nepesseg = int.Parse(sor[2].Substring(0, sor[2].Length - 1));
+            }
+            else Nepesseg = int.Parse(sor[2]);
+            Fovaros = sor[3];
+            FovarosNepesseg = int.Parse(sor[4]);
+        }
+    }
+}
